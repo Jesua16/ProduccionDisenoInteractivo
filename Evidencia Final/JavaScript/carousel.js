a3d93e4a24f3ae8940c731479d1afea4dde47525
Jesua16 <jesuasantes@gmail.comgit config --global user.email jesuasantes@gmail.com>
2023-12-01 22:08:07 -0600

    Avance de página web

---
+++ Evidencia Final/JavaScript/carousel.js
@@ -0,0 +1,26 @@
+const slides = document.querySelectorAll('.carousel-slide');
+let currentIndex = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+showSlide(currentIndex);
+
+setInterval(nextSlide, 5000); // Cambia de diapositiva automáticamente
